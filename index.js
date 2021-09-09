22cba82ea0cdb8136ca6d20d4fceb825e2d39f92
Nick <nelom3@gmail.com>
2021-09-09 14:36:34 -0700

    commit tree 58090abc1fa11be48a7fe9f990e4de0bfb99baba

--- index.js
+++ index.js
@@ -13,5 +13,8 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(" ");
+    return words.map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+  })
 }
